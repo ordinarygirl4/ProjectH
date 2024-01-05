7d77294cafa9442173d3adab45dee16ed22adfa0
ordinarygirl4 <155393648+ordinarygirl4@users.noreply.github.com>
2024-01-04 23:18:05 -0500

    Update DemoH.java

--- src/h/DemoH.java
+++ src/h/DemoH.java
@@ -3,6 +3,6 @@ package h;
 public class DemoH {
 public static void main(String[] args) {
 	System.out.println("It's almost the weekend!");
-	System.out.println("Can't wait!);
+	System.out.println("Can't wait!");
 }
 }
